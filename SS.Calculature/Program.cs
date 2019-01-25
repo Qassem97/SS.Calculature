7b0d50fb6db306cd4440f01c7df1326fc2b9ca4a
qassem.aburas@outlook.com <qassem.aburas@outlook.com>
2019-01-25 15:39:21 +0100

    to close

--- SS.Calculature/Program.cs
+++ SS.Calculature/Program.cs
@@ -12,12 +12,14 @@ namespace SS.Calculature
         {
             double inputchoice = 0;
             double a = 0, b = 0;
+            double c = 0;
+
             do
             {
                 Console.WriteLine("1. Addition 2.Subtraction 3.Maltiplicatin 4.Division");
-                Console.WriteLine("Enter your choice 1, 2, 3, 4 : ");
+                Console.Write("Enter your choice 1, 2, 3, 4 or 5 for exit: ");
                 inputchoice = double.Parse(Console.ReadLine());
-                Program p = new Program();
+
 
                 if (inputchoice > 0 && inputchoice < 5)
                 {
@@ -26,45 +28,102 @@ namespace SS.Calculature
 
                     Console.WriteLine("Enter the 2nd number: ");
                     b = double.Parse(Console.ReadLine());
+
+
                 }
+
+
+
+
                 switch (inputchoice)
                 {
                     case 1:
-                        p.Addition(a, b);
+                        Addition(a, b, c);
                         break;
                     case 2:
-                        p.Subtraction(a, b);
+                        Subtraction(a, b, c);
                         break;
                     case 3:
-                        p.Multiblcation(a, b);
+                        Multiblcation(a, b);
                         break;
                     case 4:
-                        p.Division(a, b);
+                        Division(a, b);
+                        break;
+                    case 5:
+                        Console.WriteLine("Press any key to exit");
+                        Console.ReadKey();
                         break;
 
+
+                    default:
+                        Console.WriteLine("Not valid number");
+                        break;
                 }
 
             } while (inputchoice > 0 && inputchoice < 5);
 
-        } public void Addition(double a, double b)
+        }
+
+
+        public static void Addition(double a, double b, double c)
         {
             double result = a + b;
             Console.WriteLine("Addition : a + b =" + result);
-        }
-        public void Subtraction(double a, double b)
+
+            Console.Write("Would you like to continue addition with the result? y = yes : ");
+            string yesNo = Console.ReadLine();
+
+            while (yesNo.Equals("y"))
+            {
+
+
+                Console.WriteLine("+");
+                c = double.Parse(Console.ReadLine());
+
+                result = result + c;
+                Console.WriteLine(result);
+
+                Console.Write("Would you like to continue addition with the result? y = yes : ");
+                yesNo = Console.ReadLine();
+            }
+
+
+
+        }// end of Add
+
+        public static void Subtraction(double a, double b, double c)
         {
             double result = a - b;
             Console.WriteLine("Subtraction : a - b =" + result);
-        }
-        public void Multiblcation(double a, double b)
+            Console.Write("Would you like to continue subtraction with the result? y = yes : ");
+            string yesNo = Console.ReadLine();
+
+            while (yesNo.Equals("y"))
+            {
+
+
+                Console.WriteLine("-");
+                c = double.Parse(Console.ReadLine());
+
+                result = result - c;
+                Console.WriteLine(result);
+
+                Console.Write("Would you like to continue addition with the result? y = yes : ");
+                yesNo = Console.ReadLine();
+            }
+        }//end of Sub-
+
+        public static void Multiblcation(double a, double b)
         {
             double result = a * b;
             Console.WriteLine("Multiblcation : a * b =" + result);
 
-        } public void Division(double a, double b)
+        }
+        public static void Division(double a, double b)
         {
             double result = a / b;
             Console.WriteLine("Division : a / b =" + result);
-        }//end of main
+
+        } //end of main
     }//end of class
 }//end of namespace
